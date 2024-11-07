a87f5839a06ea9150ade0f894f895cf248dbccaf
Marcos <230733@utxicotepec.edu.mx>
2024-11-07 14:24:52 -0600

    Practica 12 : Conecxión a  base de datos
     Creación de la carpeta config con el archivo db.js donde esta la conexion a la base de datos

--- index.js
+++ index.js
@@ -7,9 +7,17 @@
 import express from 'express';
 import generalRoutes from './Router/generalRouter.js'
 import userRoutes from './Router/userRouters.js'
+import db from './config/db.js'
+
 
 const app = express()
-
+//conexion a la base de datos 
+try{
+    await db.authenticate();
+    console.log('conexion exitosa a la base de datos ')
+} catch(error){
+    console.log(error)
+}
 // Configurar Templeate Engine - PUG
 app.set('view engine', 'pug')
 app.set('views','./views')

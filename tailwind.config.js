ed37f3e5065706e0f311e18dbb2c2ea944fef803
Marcos <230733@utxicotepec.edu.mx>
2024-11-08 10:08:21 -0600

    Main: Configuracion de tailwind.config.js
    Se modifico el archivo de tailwind.onfig.js para poder usar la paleta de colores enviada en el grupo de WhatsApp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./views/**/*.pug'],
+  content: ['./views/**/*.pug'], // Escanea los archivos .pug en la carpeta views
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        verde: '#648E7F',
+        azul: '#6F9CEB',
+        rosa: '#ED6B86',
+      },
+    },
   },
   plugins: [],
 }
-
